a8708ff6454bb067abbd24cd0000a77cd2d9485f
iroshan <aselairoshan11@gmail.com>
2023-01-18 20:53:01 +0530

    test commite

--- api webscrape/main.py
+++ api webscrape/main.py
@@ -1,11 +1,25 @@
-import re
-import requests
-import json
-
+# import re
+# import requests
+# import json
+import os
+# # screped data list
 # new_data = []
 # finished_data = []
 # data = []
-# last = []
+
+# #ticker data
+# top_gainer = []
+# top_losser = []
+
+# url = 'https://api.sheety.co/41dd22c7f17a225b8163baf1a4f965e2/finvizdata/sheet1'
+
+# data_1 = {
+#     "sheet1":{
+#         "topgainer" : data[1],
+#         "toplosser" : data[2]
+#     }
+# }
+
 
 # with open('scrapy_scraped_data/scrapy_scraped_data/finviz.txt','r')as file:
 #     lines = file.readlines()
@@ -20,24 +34,21 @@ import json
 #     line = re.sub("data", "",i)
 #     data.append(line)
 
-
-datas = {
-    "sheet1":{
-        "ticker":"asela"
-    }
-}
-
-
-url = 'https://api.sheety.co/41dd22c7f17a225b8163baf1a4f965e2/finvizdata/sheet1'
-response = requests.post(url, json=datas)
-
-print(response.status_code)
-    
-    
+# for i in range(38):
+#     if i <19:
+#         top_gainer.append(data[i])
         
-        
-        
+#     else:
+#         top_losser.append(data[i])
+
+
+
+
+# response = requests.post(url, json=data_1)
+
+# print(response.status_code)
+
+# with open('scrapy_scraped_data/scrapy_scraped_data/finviz.txt','r+')as file:
+#     file.truncate(0)
+      
     
-
-
-
